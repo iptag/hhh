3139cfc3622d67c26b00d96f4614915aa68d3cfd
mark elvens <36069159+yxnwh@users.noreply.github.com>
2023-11-05 12:53:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 修改openwrt登陆地址,把下面的192.168.2123.1修改成你想要的就可以了，其他的不要动
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
 # Remove the default apps
 sed -i 's/luci-app-zerotier //g' target/linux/x86/Makefile
 sed -i 's/luci-app-unblockmusic //g' target/linux/x86/Makefile
