e9835ce6d3ff9c57bee5b066e3719b7a5f4f8bd2
mark elvens <36069159+yxnwh@users.noreply.github.com>
2023-11-05 11:30:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 修改openwrt登陆地址,把下面的192.168.2123.1修改成你想要的就可以了，其他的不要动
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/kenzok8/small-package.git package/kenzo
