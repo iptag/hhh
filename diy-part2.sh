a2b38d62f8c78da40ff1813ece5e270a0bb7ea6f
mark elvens <36069159+yxnwh@users.noreply.github.com>
2023-11-12 09:49:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,3 +22,4 @@ sed -i 's/luci-app-unblockmusic //g' target/linux/x86/Makefile
 sed -i 's/luci-app-xlnetacc //g' target/linux/x86/Makefile
 sed -i 's/luci-app-ipsec-vpnd //g' target/linux/x86/Makefile
 sed -i 's/luci-app-adbyby-plus //g' target/linux/x86/Makefile
+sed -i 's/luci-app-ssr-plus //g' target/linux/x86/Makefile
