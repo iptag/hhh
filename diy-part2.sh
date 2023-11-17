4f6bf17caf9cdb4d38b8803e0209b5c67c5e03aa
mark elvens <36069159+yxnwh@users.noreply.github.com>
2023-11-17 23:03:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,6 +20,3 @@ sed -i 's/luci-app-xlnetacc //g' target/linux/x86/Makefile
 sed -i 's/luci-app-ipsec-vpnd //g' target/linux/x86/Makefile
 sed -i 's/luci-app-adbyby-plus //g' target/linux/x86/Makefile
 sed -i 's/luci-app-ssr-plus //g' target/linux/x86/Makefile
-
-# Add Passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
