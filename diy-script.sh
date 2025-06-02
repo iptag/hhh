f417a3cbe6f76c49fdd4d54724b898d8424f265e
iptag <36069159+iptag@users.noreply.github.com>
2025-06-02 13:25:58 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -56,13 +56,10 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/luci2/bin/config_gener
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
-# 修改主机名
-#sed -i "s/set system.@system\[-1\].hostname='LEDE'/set system.@system\[-1\].hostname='OpenWrt'/" package/base-files/luci2/bin/config_generate
-
 # 修改版本为编译日期
-#date_version=$(date +"%y.%m.%d")
-#orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-#sed -i "s/${orig_version}/R${date_version} by emxiong/g" package/lean/default-settings/files/zzz-default-settings
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by emxiong/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
